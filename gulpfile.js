2d8fdd3599ee3dd55ae567970706b45c18105c79
Miles Hochstein <miles.hochstein@gmail.com>
2016-01-05 14:02:26 -0800

    add jshintrc demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
 // npm install gulp gulp-mocha gulp-util
-
+ 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
 
 gulp.task('jshint', function() {
+  "use strict";
   return gulp.src('./*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default', { verbose: true }));
 });
 
 gulp.task('watch-js', function() {
+	"use strict";
     gulp.watch(['./*.js'], ['jshint']);
 });
 
-
 gulp.task('mocha', function() {
+	"use strict";
     return gulp.src(['test/*.js'], { read: false })
         .pipe(mocha({ reporter: 'list' }))
         .on('error', gutil.log);
 	});
 
 gulp.task('watch-mocha', function() {
+	"use strict";
     gulp.watch(['lib/**', 'test/**'], ['mocha']);
 });
 
